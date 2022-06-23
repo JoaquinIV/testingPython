60ea5639fafb09b074e958caabb0f57a56ef8b8b
JoaquinIV <98265381+JoaquinIV@users.noreply.github.com>
2022-06-23 16:56:04 -0300

    Update testunitario.py

--- testunitario.py
+++ testunitario.py
@@ -5,7 +5,7 @@ import unittest
 
 class Mis_test(unittest.TestCase):
 
-    #NACHO
+    #NACHO_o
     def test_titularTarjetaString(self):
         self.assertTrue(TransaccionPago("Pedro Fernandez", 1234, "En espera").esNombreTitularString())
 
